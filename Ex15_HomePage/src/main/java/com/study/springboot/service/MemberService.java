b45537fb92b55b87a04548b3060f926254657115
LeeHaNeal <harull817@naver.com>
2025-04-02 18:21:55 +0900

    2025년 04월 02일

--- Ex15_HomePage/src/main/java/com/study/springboot/service/MemberService.java
+++ Ex15_HomePage/src/main/java/com/study/springboot/service/MemberService.java
@@ -1,5 +1,7 @@
 package com.study.springboot.service;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -8,6 +10,7 @@ import com.study.springboot.repository.MemberRepository;
 
 @Service
 public class MemberService {
+	
 	@Autowired
 	MemberRepository memberRepository;
 	
@@ -15,20 +18,17 @@ public class MemberService {
 		return memberRepository.existsById(id);
 	}
 
-	   public boolean insert(Member member) {
-	        try {
-	            memberRepository.save(member);
-	            return true; 
-	        } catch (Exception e) {
-	            e.printStackTrace();
-	            return false; 
-	        }
-	    }
+	public Member insert(Member member) {
+		return memberRepository.save(member);
+	}
 
-	    public Member findById(String id) {
-	        return memberRepository.findById(id).orElse(null); 
-	    }
+	public Member login(Member member) {
+		Optional<Member> loginUser = memberRepository.findById(member.getId());
+		if(loginUser.isPresent()) {
+			return loginUser.get();
+		} else {
+			return null;
+		}
 	}
 	
-	
-
+}
